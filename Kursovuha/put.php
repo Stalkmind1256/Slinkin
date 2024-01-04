45d5bf7b8cf333941631710683d0453e9fda5224
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-01-04 14:52:02 +0500

    prefinal

--- Kursovuha/put.php
+++ Kursovuha/put.php
@@ -1,7 +1,60 @@
- <?php
-// Получение тела PUT запроса
-$body = file_get_contents('php://input');
-
-// Вывод полученного тела запроса
-echo "Received PUT data: " . $body;
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    // Получение тела PUT запроса
+    $body = file_get_contents('php://input');
+    
+    // Вывод полученного тела запроса
+    echo "Received PUT data: " . $body;
+    
+}  elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // Обработка DELETE запроса
+    echo "This is a DELETE request\n";
+    
+    // Добавьте код для выполнения удаления файла, например:
+    $fileToDelete = '/srv/http/mywebsite/test.json';
+    
+    if (file_exists($fileToDelete)) {
+        if (unlink($fileToDelete)) {
+            echo "File deleted successfully\n";
+        } else {
+            echo "Failed to delete the file\n";
+        }
+    } else {
+        echo "File does not exist\n";
+    }
+    
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Получение содержимого файла текущего скрипта
+    $scriptContent = file_get_contents(__FILE__);
+    
+    // Вывод содержимого файла
+    echo "Content of this script file:\n\n";
+    echo $scriptContent;
+    
+} elseif ($_SERVER['REQUEST_METHOD'] === 'HEAD') {
+    // Отправка HEAD запроса и получение заголовков ответа
+    $headers = get_headers(__FILE__);
+    
+    // Вывод заголовков ответа
+    echo "Headers:\n\n";
+    foreach ($headers as $header) {
+        echo $header . "\n";
+    }
+    
+} elseif ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Установка заголовков ответа для поддержки OPTIONS
+    header('Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE');
+    header('Allow: OPTIONS, GET, POST, PUT, DELETE');
+    
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получение тела POST запроса
+    $body = file_get_contents('php://input');
+    
+    // Вывод полученного тела запроса
+    echo "Received POST data: " . $body;
+    
+} else {
+    // Обработка других методов
+    echo "Unsupported request method";
+}
 ?>
--- Kursovuha/test.c
+++ Kursovuha/test.c
@@ -1,3 +1,23 @@
+
+// Создание http клиента
+
+//Клиентский алгоритм
+/*
+ * Создание сокета + 
+ * Определение адреса сервера +
+ * Подключение +
+ * Чтение и отображение сообщенией
+ * Разрыв соединения
+ * 
+ * int socket(domain, type, protocol);
+ * struct sockaddr_in  
+ * connect(sd,структура,длина структуры)
+ * 
+ * */
+
+ // Добавить или нет? ввод IP/port
+ // Нужен ли POST запрос?
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <resolv.h>
@@ -6,24 +26,60 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAXBUF 1024
 
+//bool check_valid_ip(const char* ip){
+	//	struct sockaddr_in sa;
+	//	return inet_pton(AF_INET, ip, &(sa.sin_addr)) != 0;
+	//}
+
+
 int main() {
     int socket_descriptor, bytes_read;
-
+  
+    
     struct sockaddr_in server_info;
+	char input_ip[16];
+	char input_port[6];
 
+	/*
+	 * typedet struct sockaddr_in{
+	 * short sin_family;
+	 * unsigned short  sin_port;
+	 * struct in_addr sin_addr;
+	 * char sin_zero[8];
+	 * }server_info;
+	 * 
+*/
+	printf("Enter IP address: ");
+	fgets(input_ip, sizeof(input_ip), stdin);
+    input_ip[strcspn(input_ip, "\n")] = '\0';
+    
+  /*  while(!check_valid_ip(input_ip)){
+		printf("Invalid IP addres: Please enter a valid IP address: ");
+		fgets(input_ip,sizeof(input_ip),stdin);
+		input_ip[strcspn(input_ip, "\n")] = '\0';
+	}
+    */
+    
+    
+    printf("Enter PORT address: ");
+	fgets(input_port, sizeof(input_port), stdin);
+    input_port[strcspn(input_port, "\n")] = '\0';
+    
+  
     socket_descriptor = socket(PF_INET, SOCK_STREAM, 0);
     if (socket_descriptor == -1) {
         perror("Error creating socket");
-        return 1;
+        return 1;  
     }
-
+  
     server_info.sin_family = AF_INET;
-    server_info.sin_port = htons(80);
-    server_info.sin_addr.s_addr = inet_addr("127.0.0.1");
-
+    server_info.sin_port = htons(atoi(input_port));
+    server_info.sin_addr.s_addr = inet_addr(input_ip);
+  
     int connection = connect(socket_descriptor, (struct sockaddr*)&server_info, sizeof(server_info));
     if (connection == -1) {
         perror("Error connecting to server");
@@ -31,76 +87,91 @@ int main() {
     } else {
         printf("Connected\n");
     }
-
+  
     char request[MAXBUF];
-
+  
     char response[MAXBUF];
     char method[8];
-    printf("Enter method\n");
-    printf("1. GET\n");
-    printf("2. HEAD\n");
-    printf("3. OPTIONS\n");
-    printf("4. POST\n");
-    printf("5. TRACE\n");
-    printf("6. PUT\n");
-    printf("7. DELETE\n");
-    printf("Method number: ");
-    fgets(method, sizeof(method), stdin);
-
-    if (method[strlen(method) - 1] == '\n') {
-        method[strlen(method) - 1] = '\0';
+    
+    while(true){
+		printf("Enter method\n");
+		printf("1. GET\n");
+		printf("2. HEAD\n");
+		printf("3. OPTIONS\n");
+		printf("4. POST\n");
+		printf("5. TRACE\n");
+		printf("6. PUT\n");
+		printf("7. DELETE\n");
+		printf("8. EXIT\n");
+		printf("Method number: ");
+		fgets(method, sizeof(method), stdin);
+		
+		
+		if (method[strlen(method) - 1] == '\n') {
+			method[strlen(method) - 1] = '\0';
     }
-
+    
+    
+     if (strcmp(method, "8") == 0) {
+		 printf("disconnect\n");
+            break;
+        }
+    
+    
     switch(method[0]) {
         case '1':
-            snprintf(request, sizeof(request), "GET /mywebsite/ HTTP/1.1\r\nHost: %s\r\n\r\n", "localhost");
+            snprintf(request, sizeof(request), "GET /mywebsite/put.php HTTP/1.1\r\nHost: %s\r\n\r\n", "localhost");// РАботает 
             break;
         case '2':
-            snprintf(request, sizeof(request), "HEAD /mywebsite/ HTTP/1.1\r\nHost: %s\r\n\r\n", "localhost");
+            snprintf(request, sizeof(request), "HEAD /mywebsite/put.php HTTP/1.1\r\nHost: %s\r\n\r\n", "localhost");// РАботает 
             break;
         case '3':
-            snprintf(request, sizeof(request), "OPTIONS /mywebsite/ HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost");
+            snprintf(request, sizeof(request), "OPTIONS /mywebsite/put.php HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost");// РАботает 
             break;
         case '4':
-            snprintf(request, sizeof(request), "POST / HTTP/1.1\r\nHost:%s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 5\r\n\r\nHello\r\n\r\n", "localhost");
+            snprintf(request, sizeof(request), "POST /mywebsite/put.php HTTP/1.1\r\nHost:%s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 14\r\n\r\nHello world!!!\r\n\r\n", "localhost");
             break;
         case '5':
-            snprintf(request, sizeof(request), "TRACE /mywebsite/ HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost"); //Head, Get, Post, Trace, Options
+            snprintf(request, sizeof(request), "TRACE /mywebsite/ HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost");
             break;
         case '6':
-            snprintf(request, sizeof(request), "PUT /mywebsite/put.php HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello, Server!\r\n"); // Работает через скрикт put.php
-            break;
-        case '7':
-            snprintf(request, sizeof(request), "DELETE /mywebsite/ HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost");
-            break;
+			snprintf(request, sizeof(request), "PUT /mywebsite/put.php HTTP/1.1\r\nHost: localhost\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello, Server!\r\n");
+			break;
+		case '7':
+			snprintf(request, sizeof(request), "DELETE /mywebsite/put.php HTTP/1.1\r\nHost:%s\r\n\r\n", "localhost");
+			break;	
         default:
             printf("Invalid method number\n");
             close(socket_descriptor);
             if (socket_descriptor == -1) {
                 perror("Error closing socket");
             }
-            return 1;
+            return 1;    
     }
-
-    int bytes_sent = send(socket_descriptor, request, strlen(request), 0);
-    if (bytes_sent == -1) {
-        perror("Error sending request");
-        return 1;
-    }
-
-    bytes_read = recv(socket_descriptor, response, sizeof(response), 0);
+    
+     int bytes_sent = send(socket_descriptor, request, strlen(request), 0);
+	 if (bytes_sent == -1){
+			perror("Error sending request");
+			return 1;
+	}
+    
+	bytes_read = recv(socket_descriptor, response, sizeof(response), 0);
     if (bytes_read < 0) {
         perror("Error reading data");
         return 1;
     }
-
+  
     printf("Response from server:\n");
     printf("%.*s\n", bytes_read, response);
-
+		
+	}
+		
+  
     close(socket_descriptor);
     if (socket_descriptor == -1) {
         perror("Error closing socket");
     }
+		
 
     return 0;
 }
