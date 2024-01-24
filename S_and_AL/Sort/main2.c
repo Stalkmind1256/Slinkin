d6806ded0be9bb84a1e8d5d8795ffe885a096b67
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-01-24 23:30:51 +0500

    sorted2

--- S_and_AL/Sort/main2.c
+++ S_and_AL/Sort/main2.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int sort_numbers(const void *num1, const void *num2) {
-    return *(int *) num1 - *(int *) num2;
+int sort_numbers(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
 void bin_insert_sort(void *base, size_t num, size_t size, int (*compar)(const void *, const void *)) {
-    char *array = (char *) base;
+    char *arr = (char *) base;
     char temp[size];
     for (int i = 1; i < num; i++) {
-        memcpy(temp, &array[i * size], size);
+        memcpy(temp, arr + i * size, size);
         int left = 0, right = i;
         while (left < right) {
             int middle = (left + right) / 2;
-            if (compar(temp, &array[middle * size]) > 0)
+            if (compar(temp, arr + middle * size) > 0)
                 left = middle + 1;
             else
                 right = middle;
         }
-        memmove(&array[(left + 1) * size], &array[left * size], (i - left) * size);
-        memcpy(&array[left * size], temp, size);
+        memmove(arr + (left + 1) * size, arr + left * size, (i - left) * size);
+        memcpy(arr + left * size, temp, size);
     }
 }
 
 int main() {
-    int array[5] = {5, 2, 4, 1, 3};
+    int array[5] = {57, 22, 49, 12, 38};
     printf("Bin: ");
     for (unsigned short i = 0; i < 5; i++)
         printf("%d ", array[i]);
