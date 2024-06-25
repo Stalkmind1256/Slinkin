a2200413127522400f73f928e0538b6ffe0eae35
Stalkmind1256 <119118253+Stalkmind1256@users.noreply.github.com>
2024-06-25 22:23:48 +0500

    Update block1.c

--- Praktikum/LB_6/block1.c
+++ Praktikum/LB_6/block1.c
@@ -8,48 +8,46 @@
 #define MAX_GNAME 100
 #define SHELL_INFO 100
 #define MAX_SHELLS 100
+#define MAX_MEMBERS 1000
 
 void find_group_members(char *username) {
-    FILE *passwd, *group, *shell;
+    FILE *passwd, *group;
     char buf[MAX_BUF];
     char uname[MAX_UNAME];
     char gname[MAX_GNAME];
     char shellBuf[SHELL_INFO];
     char shells[MAX_SHELLS][SHELL_INFO];
-    int uid, gid;
+    int gid;
     int num_shells = 0;
+    char unique_members[MAX_MEMBERS][MAX_UNAME];
+    int num_unique_members = 0;
 
     passwd = fopen("/home/andrey/Рабочий стол/gruppp/passwd", "r");
     if (passwd == NULL) {
-        perror("error opening passwd file");
         return;
     }
 
     group = fopen("/home/andrey/Рабочий стол/gruppp/group", "r");
     if (group == NULL) {
-        perror("error opening group file");
         fclose(passwd);
-        return;
-    }
-
-    shell = fopen("/home/andrey/Рабочий стол/gruppp/shells", "r");
-    if (shell == NULL) {
-        perror("error opening shells file");
-        fclose(passwd);
-        fclose(group);
         return;
     }
 
     // Read valid shells
+    FILE *shell = fopen("/home/andrey/Рабочий стол/gruppp/shells", "r");
+    if (shell == NULL) {
+        fclose(passwd);
+        fclose(group);
+        return;
+    }
     while (fgets(buf, sizeof(buf), shell) != NULL) {
         strtok(buf, "\n");
         strncpy(shells[num_shells], buf, SHELL_INFO);
-        printf("Shell %d: %s\n", num_shells, shells[num_shells]);
         num_shells++;
     }
     fclose(shell);
 
-    // Find the user's primary group ID and shell
+    // Find the user's primary group ID
     int user_found = 0;
     while (fgets(buf, sizeof(buf), passwd) != NULL && !user_found) {
         char *line = strdup(buf);
@@ -58,8 +56,6 @@ void find_group_members(char *username) {
         while (token != NULL) {
             if (field == 0) {
                 strncpy(uname, token, MAX_UNAME);
-            } else if (field == 2) {
-                uid = atoi(token);
             } else if (field == 3) {
                 gid = atoi(token);
             }
@@ -67,30 +63,8 @@ void find_group_members(char *username) {
             field++;
         }
 
-        char *last_colon = strrchr(buf, ':');
-        if (last_colon != NULL) {
-            strncpy(shellBuf, last_colon + 1, SHELL_INFO);
-            shellBuf[strcspn(shellBuf, "\n")] = 0;
-        }
-
         if (strcmp(uname, username) == 0) {
             user_found = 1;
-            printf("username: %s\n", uname);
-            printf("uid: %d\n", uid);
-            printf("gid: %d\n", gid);
-            printf("shell: %s\n", shellBuf);
-            int is_service_user = 1;
-            for (int i = 0; i < num_shells; i++) {
-                if (strcmp(shellBuf, shells[i]) == 0) {
-                    is_service_user = 0;
-                    break;
-                }
-            }
-            if (is_service_user) {
-                printf("This is a service user.\n");
-            } else {
-                printf("This is a real user.\n");
-            }
         }
         free(line);
     }
@@ -104,7 +78,6 @@ void find_group_members(char *username) {
 
     // Find all groups the user belongs to
     char group_buf[MAX_BUF];
-    printf("Groups:\n");
     while (fgets(group_buf, sizeof(group_buf), group) != NULL) {
         char *group_line = strdup(group_buf);
         char *group_token = strtok(group_line, ":");
@@ -124,12 +97,9 @@ void find_group_members(char *username) {
         }
 
         if (group_gid == gid || (group_members && strstr(group_members, username))) {
-            printf("Group name: %s\n", gname);
-            printf("Group members:\n");
-            // Find and print all group members
+            // Find all group members
             FILE *passwd2 = fopen("/home/andrey/Рабочий стол/gruppp/passwd", "r");
             if (passwd2 == NULL) {
-                perror("error opening passwd file");
                 free(group_line);
                 fclose(group);
                 return;
@@ -140,6 +110,7 @@ void find_group_members(char *username) {
                 char member_name[MAX_UNAME];
                 int member_gid = -1;
                 int field = 0;
+                int is_duplicate = 0;
                 while (token != NULL) {
                     if (field == 0) {
                         strncpy(member_name, token, MAX_UNAME);
@@ -162,7 +133,20 @@ void find_group_members(char *username) {
                             break;
                         }
                     }
-                    printf("  %s (%s)\n", member_name, is_service_user ? "service user" : "real user");
+                    // Check for duplicates before adding to the list
+                    for (int i = 0; i < num_unique_members; i++) {
+                        if (strcmp(member_name, unique_members[i]) == 0) {
+                            is_duplicate = 1;
+                            break;
+                        }
+                    }
+                    if (!is_duplicate) {
+                        strncpy(unique_members[num_unique_members], member_name, MAX_UNAME);
+                        num_unique_members++;
+
+                        // Output with type of user
+                        printf("%s - %s\n", member_name, is_service_user ? "Service User" : "Real User");
+                    }
                 }
                 free(line);
             }
